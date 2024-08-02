24de77484281510ddeabadeeb27046afa4a309af
S4KZ <samuelvini04@gmail.com>
2024-08-02 13:33:27 -0300

    mudanca de background

--- pages/log/welcome/index.js
+++ pages/log/welcome/index.js
@@ -31,7 +31,7 @@ const styles = StyleSheet.create({
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'red'
+       
     },
     image: {
         height: 350,
