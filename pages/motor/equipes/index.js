915626663078a7532959a73280d3715bc041b4af
Samuel Vinícius <166819602+S4KZ@users.noreply.github.com>
2024-09-22 11:39:47 -0300

    Add files via upload

--- pages/motor/equipes/index.js
+++ pages/motor/equipes/index.js
@@ -1,27 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Image, ScrollView, SafeAreaView } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { useRoute } from '@react-navigation/native';
 import config from '../../../config/config.json';
 import Adicionar from './adicionar';
 import Excluir from './excluir';
 
-const ilusEqui = require("../../../assets/icons/ilustra-Equipes.png"); // O ícone
+const ilusEqui = require("../../../assets/icons/ilustra-Equipes.png");
 
 function EquipesTela() {
     const navigation = useNavigation();
     const route = useRoute();
-    const { userId } = route.params || {}; // Garante que userId é acessado de forma segura
+    const { userId } = route.params || {};
     const [equipes, setEquipes] = useState([]);
+
+    //recarrega a tela quando foca
     useEffect(() => {
         if (userId) {
             handleEquipe(userId);
         }
-    }, [userId]);
 
-    async function handleEquipe(userId) {
+        const unsubscribe = navigation.addListener('focus', () => {
+            handleEquipe(userId);
+        });
+
+        return unsubscribe;
+    }, [userId, navigation]);
+    //fim do reload
+
+    const handleEquipe = async (userId) => {
         try {
             const response = await fetch(config.urlRootNode + '/equipe', {
                 method: 'POST',
@@ -29,37 +37,33 @@ function EquipesTela() {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({
-                    pasIdEquipe: userId, // Use o parâmetro correto para a equipe
-                }),
+                body: JSON.stringify({ pasIdEquipe: userId }),
             });
 
             const data = await response.json();
-            const results = data.results; // Acessa a array de resultados
+            const results = data.results;
 
-            if (Array.isArray(results)) { // Verifica se results é um array
-                setEquipes(results); // Atualiza o estado ou faz o que for necessário com os resultados
+            if (Array.isArray(results)) {
+                setEquipes(results);
             } else {
                 console.error('A resposta não contém uma array de resultados.');
             }
         } catch (error) {
             console.error('Erro ao buscar dados da equipe:', error);
         }
-    }
+    };
 
     return (
         <ScrollView>
             <View style={styles.container}>
                 <Image source={ilusEqui} style={styles.ilustra} />
                 <View style={styles.box3}>
-
                     <View style={styles.box1}>
                         <Text style={styles.title}>Turma da manhã</Text>
                         <Text style={styles.subtitle}>Horário da chegada:  7:20h</Text>
                         <Text style={styles.subtitle}>Horário da saída:   15:30h</Text>
                         <Text style={styles.subtitle}>Local: Escola Etec Alfredão, Rua José Alves, 450</Text>
                     </View>
-
 
                     <View style={styles.box}>
                         <TouchableOpacity style={styles.item}>
@@ -70,18 +74,17 @@ function EquipesTela() {
                         <View style={styles.row2}>
                             {equipes.length > 0 ? (
                                 equipes.map((item, index) => (
-
                                     <View key={index} style={styles.infobox}>
-                                        <Text style={styles.label}> {item.useNome}     </Text>
-                                        <Text style={styles.info}>  {item.useEmail}</Text>
+                                        <Text style={styles.label}>{item.useNome}</Text>
+                                        <Text style={styles.info}>{item.useEmail}</Text>
                                     </View>
                                 ))
                             ) : (
                                 <Text style={styles.info}>Nenhum membro encontrado</Text>
                             )}
                         </View>
-                        
                     </View>
+
                     <TouchableOpacity style={styles.botaoConf} onPress={() => navigation.navigate('Menu')}>
                         <Text style={styles.texto}>Editar Equipes</Text>
                     </TouchableOpacity>
@@ -97,16 +100,16 @@ function Menu() {
         <View style={styles.container}>
             <ScrollView>
                 <SafeAreaView>
-                    <TouchableOpacity style={styles.botao} onPress={() => navigation.navigate('Adicionar')} >
+                    <TouchableOpacity style={styles.botao} onPress={() => navigation.navigate('Adicionar')}>
                         <Text style={styles.text}>Adicionar integrante</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.botao} onPress={() => navigation.navigate('Excluir')} >
+                    <TouchableOpacity style={styles.botao} onPress={() => navigation.navigate('Excluir')}>
                         <Text style={styles.text}>Excluir integrante</Text>
                     </TouchableOpacity>
                 </SafeAreaView>
             </ScrollView>
-            <TouchableOpacity style={styles.botaoConf} onPress={() => navigation.navigate('EquipesTela')} >
+            <TouchableOpacity style={styles.botaoConf} onPress={() => navigation.navigate('EquipesTela')}>
                 <Text style={styles.texto}>Voltar para Equipes</Text>
             </TouchableOpacity>
         </View>
@@ -116,14 +119,14 @@ function Menu() {
 export default function Equipes() {
     const Stack = createNativeStackNavigator();
     const route = useRoute();
-    const { userId } = route.params || {}; // Garante que userId é acessado de forma segura
-    
+    const { userId } = route.params || {};
+
     return (
         <Stack.Navigator>
             <Stack.Screen name="EquipesTela" initialParams={{ userId }} component={EquipesTela} options={{ headerShown: false }} />
             <Stack.Screen name="Menu" initialParams={{ userId }} component={Menu} options={{ headerShown: false }} />
-            <Stack.Screen name='Adicionar' initialParams={{ userId }} component={Adicionar}  options={{ headerShown: false }} />
-            <Stack.Screen name='Excluir' initialParams={{ userId }} component={Excluir}  options={{ headerShown: false }} />
+            <Stack.Screen name='Adicionar' initialParams={{ userId }} component={Adicionar} options={{ headerShown: false }} />
+            <Stack.Screen name='Excluir' initialParams={{ userId }} component={Excluir} options={{ headerShown: false }} />
         </Stack.Navigator>
     );
 }
@@ -135,23 +138,19 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#ffff',
     },
-    
-  box3: {
-    flexDirection:'column',
-    padding: 40,
-    maxHeight:1900,
-    minHeight:900,
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
-    backgroundColor: '#FFF',
-    shadowColor: '#000',
-    shadowOffset: { width:0, height:10 },
-    shadowRadius: 1.3,
-    elevation: 25,
-    marginBottom:30,
-    top:20,
-    
-  },
+    box3: {
+        flexDirection: 'column',
+        padding: 40,
+        backgroundColor: '#FFF',
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 10 },
+        shadowRadius: 1.3,
+        elevation: 25,
+        marginBottom: 30,
+        top: 20,
+    },
     ilustra: {
         width: 450,
         height: 340,
@@ -168,13 +167,10 @@ const styles = StyleSheet.create({
     },
     icon: {
         marginRight: 10,
-       
     },
     box: {
         backgroundColor: "#FFF",
         padding: 20,
-        maxWidth:380,
-        minWidth:350,
         borderRadius: 10,
         alignItems: "center",
         shadowColor: '#000',
@@ -188,9 +184,7 @@ const styles = StyleSheet.create({
     box1: {
         padding: 20,
         borderRadius: 10,
-        marginLeft:10,
-        maxWidth:350,
-        minWidth:100,
+        maxWidth: 350,
         backgroundColor: '#FFFFFF',
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 15 },
@@ -206,10 +200,8 @@ const styles = StyleSheet.create({
     },
     infobox: {
         width: '100%',
-        alignItems: 'stretch',
         marginBottom: 30,
-        marginLeft:30,
-
+        marginLeft: 30,
     },
     title: {
         fontSize: 24,
@@ -232,7 +224,6 @@ const styles = StyleSheet.create({
         fontSize: 16,
         textAlign: 'left',
     },
-
     botaoConf: {
         width: 290,
         height: 50,
@@ -240,8 +231,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 10,
-        paddingHorizontal: 20,
-        paddingVertical: 10,
         marginHorizontal: 25,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 15 },
@@ -250,7 +239,7 @@ const styles = StyleSheet.create({
         elevation: 5,
         marginTop: 5,
         marginBottom: 110,
-        marginLeft:40,
+        marginLeft: 40,
     },
     botao: {
         width: 250,
@@ -259,12 +248,9 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 10,
-        paddingHorizontal: 20,
-        paddingVertical: 10,
         marginHorizontal: 25,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 15 },
-       
         shadowOpacity: 0.8,
         shadowRadius: 4,
         elevation: 5,
