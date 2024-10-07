2951017ba4df7c397659b497a0c532321c1c2c60
MigSilverS <migupoke@gmail.com>
2024-10-07 19:14:31 -0300

    Atualização do mapa

--- screens/maps/index.js
+++ screens/maps/index.js
@@ -1,14 +1,14 @@
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import { StyleSheet, View, Text, ActivityIndicator } from "react-native";
 import * as Location from 'expo-location';
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import GooglePlacesInput from "./googlePlacesAutocomplete";
-import { StatusBar } from "expo-status-bar";
 
 export default function GoogleMapsScreen() {
     const [location, setLocation] = useState(null);
     const [errorMsg, setErrorMsg] = useState(null);
     const [loading, setLoading] = useState(true); // Para controlar o carregamento do mapa
+    const mapRef = useRef(null);
 
     useEffect(() => {
         (async () => {
@@ -22,7 +22,7 @@ export default function GoogleMapsScreen() {
             let location = await Location.getCurrentPositionAsync({});
             setLocation(location);
             setLoading(false); // Parar o loading quando a localização for obtida
-            console.log(location);
+            // console.log(location); //Fala a localização atual no console log
         })();
     }, []);
 
@@ -32,8 +32,11 @@ export default function GoogleMapsScreen() {
             timeInterval: 1000,
             distanceInterval: 1,
         }, (response) => {
-            console.log('Nova localização: ', response);
+            // console.log('Nova localização: ', response); // Mostra a localização após o movimento/ ou atualiza a localização
             setLocation(response);
+            mapRef.current?.animateCamera({
+                center: response.coords
+            })
         });
     }, []);
 
@@ -58,6 +61,7 @@ export default function GoogleMapsScreen() {
         <View style={styles.container}>
             
             <MapView
+                ref={mapRef}
                 provider={PROVIDER_GOOGLE}
                 style={styles.map}
                 initialRegion={{
