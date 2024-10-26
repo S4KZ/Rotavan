465512534c71fbe4487a0364ff4e8abaf8221728
Samuel Vinícius <166819602+S4KZ@users.noreply.github.com>
2024-10-26 18:20:05 -0300

    função para a validação de equipe
    Agora o motorista pode cadastrar nova equipe

--- pages/motor/equipes/cadastrar/index.js
+++ pages/motor/equipes/cadastrar/index.js
@@ -1,11 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, ScrollView, TextInput } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Picker } from '@react-native-picker/picker';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import config from '../../../../config/config.json';
+
 
 export default function CadastrarEquipe() {
-  const [selectedSchool, setSelectedSchool] = useState(''); 
+  const navigation = useNavigation();
+  const route = useRoute();
+  const { userId } = route.params || {};
+  // console.log(userId);
+  const [selectedTurno, setSelectedTurno] = useState(''); // Estado para o valor selecionado do Picker
+  const [turnos, setTurnos] = useState([]);
+  const [nomeEquipe, setNomeEquipe] = useState('');
   const [cep, setCep] = useState('');
   const [endereco, setEndereco] = useState({
     logradouro: '',
@@ -13,6 +22,44 @@ export default function CadastrarEquipe() {
     cidade: '',
     uf: ''
   });
+
+
+  useEffect(() => {
+    if (userId) {
+      HandleTurno(userId);
+    }
+    const unsubscribe = navigation.addListener('focus', () => {
+      HandleTurno(userId);
+    });
+
+    return unsubscribe;
+  }, [userId, navigation]);
+
+
+
+  // Função para buscar os turnos
+  const HandleTurno = async (userId) => {
+    try {
+      const ress = await fetch(config.urlRootNode + '/turno', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ id: userId }),
+      });
+      const data = await ress.json();
+      const turnos = data.turnos;
+      if (Array.isArray(turnos)) {
+        setTurnos(turnos);
+      } else {
+        console.error('A resposta não contém uma array de resultados.');
+      }
+    } catch (error) {
+      console.error('Erro ao buscar turnos:', error);
+    }
+  };
+
 
   async function buscarEndereco(cep) {
     try {
@@ -28,41 +75,87 @@ export default function CadastrarEquipe() {
           uf: data.uf
         });
       }
+
     } catch (error) {
       alert('Erro ao buscar o endereço. Tente novamente.');
     }
   }
 
   async function handleCadastrarEquipe() {
-    if (selectedSchool === '') {
-      alert('Por favor, selecione o endereço e turno da equipe.');
+    if (selectedTurno === '' || nomeEquipe === '' || cep === '' || !endereco.logradouro) {
+      alert('Por favor, preencha todos os campos obrigatórios.');
       return;
     }
-    alert(`Equipe cadastrada com sucesso! Endereço e turno: ${selectedSchool}`);
+    try {
+      const response = await fetch(config.urlRootNode + '/cadEquip', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          nomeEquipe,
+          selectedTurno,
+          cep,
+          endereco,
+          userId,
+        }),
+      });
+
+      const data = await response.json();
+      console.log(data);
+      if(data.results === true){
+        alert('Equipe cadastrada com sucesso!');
+      }else{
+        alert('Não foi possível cadastrar a equipe. Tente novamente.');
+      }
+    } catch (error) {
+      console.error('Erro ao cadastrar equipe:', error);
+      alert('Erro ao cadastrar equipe. Tente novamente.');
+    }
+    
   }
+
+  const onTurnoChange = (itemValue) => {
+
+    if (itemValue) {
+      console.log('Turno selecionado:', itemValue); // Adicione esse console.log para imprimir o valor de itemValue
+      setSelectedTurno(itemValue);
+      // handleEquipe(itemValue);
+      //OBS: LEMBRAR-SE QUE itemValue é o id do turno selecionado
+    }
+  };
 
   return (
     <ScrollView>
       <SafeAreaView style={styles.container}>
-             <Text style={styles.title}>Cadastrar Equipe</Text>
-             {/* <Icon name="users" size={30} color="#F6B628" style={styles.icon} />
+        <Text style={styles.title}>Cadastrar Equipe</Text>
+        {/* <Icon name="users" size={30} color="#F6B628" style={styles.icon} />
     */}
 
         <View style={styles.card}>
           <Picker
-            selectedValue={selectedSchool}
+            selectedValue={selectedTurno}
             style={styles.picker}
-            onValueChange={(itemValue) => setSelectedSchool(itemValue)}
+            onValueChange={(itemValue) => onTurnoChange(itemValue)} // Chama a função quando um turno é selecionado
           >
-            <Picker.Item label="Selecione o turno" value="" />
-            <Picker.Item label="Turno Manhã" value="Turno-manhã" />
-            <Picker.Item label="Turno Tarde" value="Turno-tarde" />
-            <Picker.Item label="Turno Noite" value="Turno-noite" />
+            <Picker.Item label='Nenhum turno selecionado' value='' />
+            {turnos.length > 0 ? (
+              turnos.map((turno, index) => (
+                <Picker.Item key={index} label={turno.turPeriodo} value={turno.turId} />
+              ))
+            ) : (
+              <Picker.Item label='Nenhum turno disponível' value='' />
+            )}
           </Picker>
         </View>
 
         <View style={styles.form}>
-          <TextInput style={styles.input} placeholder="Nome da equipe" />
+          <TextInput style={styles.input}
+            placeholder="Nome da equipe"
+            value={nomeEquipe} // Ligando o estado ao TextInput
+            onChangeText={(text) => setNomeEquipe(text)} // Atualiza o nome da equipe no estado
+          />
         </View>
 
         <View style={styles.form}>
@@ -186,7 +279,7 @@ const styles = StyleSheet.create({
     height: 50,
     width: 300,
     margin: 1,
-    borderColor:'#CCCCCC',
+    borderColor: '#CCCCCC',
     borderRadius: 7,
     paddingHorizontal: 20,
     paddingVertical: 10,
