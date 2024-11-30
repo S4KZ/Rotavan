1ffd56b854439a357113ba6e6e6150cf54765281
Samuel Vinícius <166819602+S4KZ@users.noreply.github.com>
2024-11-29 22:04:41 -0300

    Update index.js

--- pages/log/Sair/index.js
+++ pages/log/Sair/index.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { Alert } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
-import { View, Button } from 'react-native';
-import Welcome from '../welcome'
 
-export default function Sair(){
+export default function Sair() {
     const navigation = useNavigation();
 
-    const Exit = ()=>{
-        Alert.alert("Você saiu..");
-        navigation.navigate('Welcome');
-    }
+    useEffect(() => {
+        // Função que será executada uma vez ao montar o componente
+        const handleExit = () => {
+            Alert.alert("Você saiu..");
+            navigation.navigate('Welcome'); // Navega para a tela 'Welcome'
+        };
 
-    return (
-        Exit()
-    );
+        handleExit();
+    }, [navigation]); // Executa o efeito quando o componente é montado e navegação está disponível
+
+    return null; // Não é necessário renderizar nada
 }
